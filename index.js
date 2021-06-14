eba6311cfb86b3cf76886772d16ddc3f0cc24645
Dana Allam <dana.allam99@hotmail.com>
2021-06-14 12:23:56 +0300

    step 4

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ app.get("/test", function (req, res) {
 app.get("/time", function (req, res) {
   res.send({ status: 200, message: time });
 });
+
+app.get("/hello/:id", function (req, res) {
+  const { id } = req.params;
+  res.send({ status: 200, message: "Hello, " + id });
+});
+
+app.get("/hello/", function (req, res) {
+  const { id } = req.params;
+  res.send({ status: 200, message: "Hello, unknown" });
+});
+
+app.get("/search", function (req, res) {
+  const data = req.query.s;
+  if (data)
+    res.send({
+      status: 200,
+      message: "ok",
+      data: data,
+    });
+  else
+    res.status(500).send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search",
+    });
+});
