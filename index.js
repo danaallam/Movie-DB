701ff9375823f33b5a6ce25b3262bcf6e7b1a264
Dana Allam <dana.allam99@hotmail.com>
2021-06-14 10:41:40 +0300

    step 3

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const today = new Date();
+const time = today.getHours() + ":" + today.getMinutes();
 
 app.get("/", (req, res) => {
   res.send("ok");
 });
 
 app.listen(3000, () => console.log("Successful response"));
+
+app.get("/test", function (req, res) {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", function (req, res) {
+  res.send({ status: 200, message: time });
+});
